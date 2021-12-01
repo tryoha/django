9078ded81cb30eb33178c0388d8094889a30a8be
tryoha <e.tregubov@gmail.com>
2021-12-01 18:55:29 +0300

    move person in client section

--- crm/admin.py
+++ crm/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
 from .models import Person, Client
 
 
-admin.site.register(Person)
-admin.site.register(Client)
+class PersonInlne(admin.TabularInline):
+    model = Person
+    extra = 1
+
+
+class ClientAdmin(admin.ModelAdmin):
+    inlines = [PersonInlne]
+    list_display = ('name', 'status', 'price_type', 'delivery')
+    search_fields = ['name']
+
+
+admin.site.register(Client, ClientAdmin)
--- crm/models.py
+++ crm/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
 PRICE = (
     ("И", "Интернет"),
     ("Р", "Розница"),
@@ -27,28 +26,8 @@ CLIENT_STATUS = (
 )
 
 
-class Person(models.Model):
-    first_name = models.CharField('Имя', max_length=30)
-    last_name = models.CharField("Фамилия", max_length=30, blank=True)
-    PERSON_JOBS = [
-        ("Учредитель", "Учредитель"),
-        ("Администратор", "Администратор"),
-        ("Сверки", "Сверки"),
-    ]
-    position = models.CharField(
-        'Должность', max_length=15, choices=PERSON_JOBS, blank=True)
-    email = models.EmailField("Почта", max_length=254, blank=True)
-    phone = PhoneNumberField("Телефон", blank=True)
-    exist = models.BooleanField("Работает", default=True)
-    about = models.TextField("Примечания", blank=True)
-
-    def __str__(self):
-        return f'{self.first_name} {self.last_name}'
-
-
 class Client(models.Model):
     name = models.CharField("Название", max_length=50)
-    contact = models.ManyToManyField(Person, blank=True)
     adress = models.CharField("Адрес", max_length=254, blank=True)
     phone = PhoneNumberField("Телефон", blank=True)
     status = models.CharField("Cтатус", max_length=20,
@@ -58,7 +37,27 @@ class Client(models.Model):
     price_type = models.CharField("Тип цен", max_length=1, choices=PRICE)
     delivery = models.CharField(
         "Развозка", max_length=12, choices=DELIVERY_REGIONS, blank=True)
-    slug = models.SlugField("Ссылка", unique=True, max_length=40)    
+    slug = models.SlugField("Ссылка", unique=True, max_length=40)
 
     def __str__(self):
         return self.name
+
+
+class Person(models.Model):
+    first_name = models.CharField('Имя', max_length=30)
+    last_name = models.CharField("Фамилия", max_length=30, blank=True)
+    PERSON_JOBS = [
+        ("Учредитель", "Учредитель"),
+        ("Администратор", "Администратор"),
+        ("Сверки", "Сверки"),
+    ]
+    contact = models.ForeignKey(Client, on_delete=models.CASCADE, blank=True)
+    position = models.CharField(
+        'Должность', max_length=15, choices=PERSON_JOBS, blank=True)
+    email = models.EmailField("Почта", max_length=254, blank=True)
+    phone = PhoneNumberField("Телефон", blank=True)
+    exist = models.BooleanField("Работает", default=True)
+    # about = models.TextField("Примечания", blank=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
