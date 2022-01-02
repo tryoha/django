c70966b89d594530533a8b34a46ddb2e94e3c81f
tryoha <e.tregubov@gmail.com>
2022-01-02 17:03:00 +0300

    gunicorn

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,7 +1,7 @@
 import multiprocessing
 
-command = '/home/tryoha/django/bin/gunicorn'
-pythonpath = '/home/tryoha/django/project/'
+command = '/home/barmazon/projects/django/.env/bin/gunicorn'
+pythonpath = '/home/barmazon/projects/django/project/'
 bind = '127.0.0.1:8882'
 # workers = 4
 workers = multiprocessing.cpu_count()
