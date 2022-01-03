749a58d5bd5b0650c25419d3da8cd52052b1dc2e
tryoha <e.tregubov@gmail.com>
2022-01-04 01:35:03 +0300

    bl title

--- crm/views.py
+++ crm/views.py
@@ -7,9 +7,11 @@ from .models import Client
 
 class IndexView(generic.ListView):
     model = Client
+    context_object_name = 'client_list'
     template_name = "crm/index.html"
 
 
 class DetailView(generic.DetailView):
     model = Client
+    context_object_name = 'client'
     template_name = "crm/detail.html"
